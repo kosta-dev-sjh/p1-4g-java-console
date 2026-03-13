f356d5b5d8fa42fbfc7c75b72ed693fe01622d2b
DevAZI <jmini515@naver.com>
2026-03-13 12:11:37 +0900

    [Feat: ADD](P1JC-67)Random Util Code Added
    Random Util Code Added & Dice Util Deleted

--- src/com/kosta/console_rpg/util/RandomUtil.java
+++ src/com/kosta/console_rpg/util/RandomUtil.java
@@ -1,5 +1,44 @@
 package com.kosta.console_rpg.util;
 
-public class RandomUtil {
+import java.util.HashMap;
+import java.util.Random;
 
+public class RandomUtil {
+	
+	//start이상 end미만값의 int 값 반환
+	public int randomArrange(int start, int end) {	  
+		Random random = new Random();	 
+		int result = random.nextInt(start,end);	 
+	 
+		return result;
+	} 
+	//1~6의 int형 정수 반환
+	public int diceRoll() {
+		return randomArrange(1,7);
+	}
+	//{4=⚃}의 map으로 반환
+	public HashMap<Integer,String> inhencedDiceRoll(){
+		int result= diceRoll();
+		HashMap<Integer,String> dice = new HashMap<>(); 
+		switch(result) {
+			case 1: dice.put(1, "⚀");
+			break;
+			case 2: dice.put(2, "⚁");
+			break;
+			case 3: dice.put(3, "⚂");
+			break;
+			case 4: dice.put(4, "⚃");
+			break;
+			case 5: dice.put(5, "⚄");
+			break;
+			case 6: dice.put(6, "⚅");
+			break;
+			default: System.out.println("error");
+			break;
+			
+				
+		}			
+		return dice;
+		 
+	 }
 }
