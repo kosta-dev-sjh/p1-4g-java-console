3f1c7973ae4c7d2774f51a2bf5d50ef217d7552d
Jeong Hyun Song <109492769+Song991123@users.noreply.github.com>
2026-03-13 09:26:24 +0900

    Revise InventoryController class comment
    Update the comment to reflect the purpose of the InventoryController.

---
+++ src/com/kosta/console_rpg/controller/InventoryController.java
@@ -0,0 +1,12 @@
+package com.kosta.console_rpg.controller;
+/**
+ * 인벤토리 내 아이템 조회, 장착/해제, 사용 기능 흐름을 제어하는 컨트롤러
+ *
+ * 작성자      : 
+ * 생성일      : 
+ * 최종 수정자 : 
+ * 최종 수정일 : 
+ */
+public class InventoryController {
+
+}
