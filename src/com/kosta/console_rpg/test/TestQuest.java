d19756ea3ee77fa4fc95e44dc47f44d5ed29fdbd
DevAZI <80841284+DevAZI@users.noreply.github.com>
2026-03-15 23:18:55 +0900

    Feature/p1 jc 100 quest select (#29)
    * [Feat: FIX](P1JC-81)MonsterDAO/Impl, BattleService/Controller FIX
    
    ## 작업 내용
    몬스터 조회 관련
    - 메서드명 selectMonstersByStage → selectMonsterByStage 하나만 수정되어있어 일괄적 수정
    ---
    
    ## 주요 변경 사항
    - monster 관련 db 출력 확인
    - 추후 hero 관련 데이터 추가시 maxClearStage 관련 기능 추가
    ---
    
    ## 테스트 여부
    - [ O  ] 정상 동작 확인
    
    ---
    
    * [Feat: ADD](P1JC-100) Quest etc. select all feature add
    
    ## 작업 내용
    퀘스트 heroId 바탕으로 전체 퀘스트 내용 출력
    - questingDTO삭제 > questDTO에 합병 - 기능을 더 간단하게 구현 가능하기에 변경
    ---
    
    ## 주요 변경 사항
    - quest select all 기능 추가
    - questingDTO 삭제 > questDTO에 합병
    ---
    
    ## 테스트 여부
    - [ O  ] 정상 동작 확인
    
    ---

---
+++ src/com/kosta/console_rpg/test/TestQuest.java
@@ -0,0 +1,9 @@
+package com.kosta.console_rpg.test;
+
+import com.kosta.console_rpg.view.QuestView;
+
+public class TestQuest {
+    public static void main(String[] args) {
+        QuestView.showQuestLists(1);
+    }
+}
