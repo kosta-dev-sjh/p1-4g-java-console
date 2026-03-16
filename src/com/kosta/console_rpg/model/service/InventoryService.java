d1e47dc7268a4d2d370a56de2bfb7d1aabb5db01
Leozzun <syt09159@gmail.com>
2026-03-16 12:33:28 +0900

    Feature/inventory/shop/hero (#33)
    * 히어로/인벤토리/상점
    
    * Format code for consistency in HeroController
    
    * Add documentation for HeroDAO interface
    
    Added documentation comment for HeroDAO interface.
    
    * Refactor HeroDAOImpl for code consistency
    
    * Refactor HeroService methods for consistency
    
    ---------
    
    Co-authored-by: Jeong Hyun Song <109492769+Song991123@users.noreply.github.com>

--- src/com/kosta/console_rpg/model/service/InventoryService.java
+++ src/com/kosta/console_rpg/model/service/InventoryService.java
@@ -1,5 +1,43 @@
 package com.kosta.console_rpg.model.service;
 
-public class InventoryService {
+import com.kosta.console_rpg.model.dao.InventoryDAOImpl;
+import com.kosta.console_rpg.model.dto.InventoryDTO;
 
+import java.util.List;
+
+import com.kosta.console_rpg.exception.GameException;
+import com.kosta.console_rpg.model.dao.InventoryDAO;
+
+public class InventoryService {
+	
+	private InventoryDAO inventoryDAO = new InventoryDAOImpl();
+	
+	private int heroId;
+	
+	public List<InventoryDTO> showInventory() throws GameException {
+
+	    List<InventoryDTO> list = inventoryDAO.selectInventoryByHeroId(heroId);
+
+	    if(list.isEmpty()) {
+	        throw new GameException("인벤토리가 비어있습니다.");
+	    }
+
+	    return list;
+	} 
+	
+	public void equipItem(int itemId) throws GameException {
+	
+		inventoryDAO.equipItem(heroId, itemId);
+		
+	}
+	
+	public void unequipItem(int itemId) throws GameException {
+		
+		inventoryDAO.unequipItem(heroId, itemId);
+		
+	}
+	
+	public void usePotion(int inventoryId) {
+		
+	}
 }
--- src/com/kosta/console_rpg/view/HeroView.java
+++ src/com/kosta/console_rpg/view/HeroView.java
@@ -2,4 +2,15 @@ package com.kosta.console_rpg.view;
 
 public class HeroView {
 
+	public static void createHeroView() {
+		
+	}
+	
+	public static void showHeroInfo() {
+
+    }
+	
+	public static void skillMenu() {
+		
+	}
 }
