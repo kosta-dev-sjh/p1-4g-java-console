4dd1543138efd6f5666269a6b05f04bc64c29461
DevAZI <80841284+DevAZI@users.noreply.github.com>
2026-03-19 21:04:54 +0900

    feat Fix hero delete (#74)

--- src/com/kosta/console_rpg/model/dao/InventoryDAO.java
+++ src/com/kosta/console_rpg/model/dao/InventoryDAO.java
@@ -52,5 +52,6 @@ public interface InventoryDAO {
 	 * 인벤토리에 아이템 등록
 	 */
 	void insertItem(int heroId, int itemId) throws GameException;
-	
+
+	void deleteInventory(int heroId) throws GameException;
 }
